6c20997ecdfb0240197f5f89cc205cd6bf10472a
BerlinWong <wzysws@gmail.com>
2023-12-25 18:28:23 +0800

    :lipstick: 功能开发 | 更新物料以及API parampro.js

---
+++ src/store/modules/customer/modules/parampro.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+export default {
+  namespaced: true,
+  state: {
+    // 用户信息
+    taskList: null,
+    stockList: []
+  },
+  mutations: {
+    taskList(state, data) {
+      state.taskList = data
+    },
+    stockList(state, data) {
+      state.stockList = data
+    }
+  },
+  actions: {
+    // 封装一个 ajax 方法
+    async getUnfinishTasks(context, payload) {
+      // 发起异步请求
+      // 查询所有未完成任务
+      const response = await axios.post('/get_individual/unfinished', payload)
+      // 提交 mutation 来更新 state
+      context.commit('taskList', response.data)
+      return response.data // 可以返回响应数据
+    },
+    async inputSearchQuery(context, payload) {
+      // 发起异步请求
+      const response = await axios.post('/get_all_stock_by_szorsh_ws', payload)
+      // 提交 mutation 来更新 state
+      context.commit('stockList', response.data)
+      return response.data // 可以返回响应数据
+    }
+  }
+}
